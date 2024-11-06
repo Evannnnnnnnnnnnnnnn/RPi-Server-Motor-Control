c1ab39021a8f238269de11a3878f3cf200283c3c
EPF Student - Evan <evan.pasteau@uwe.ac.uk>
2024-11-06 15:14:44 +0000

    first commit

---
+++ RPiServer.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__" :
+    print("\033cStarting ...\n") # Clear Terminal
+
+import socket
+import time
+
+buffer_size = 1024
+serverPort = 2222
+serverIP = '192.168.0.152'
+messageFromServer = 'Hello World'
+
+
+
+
