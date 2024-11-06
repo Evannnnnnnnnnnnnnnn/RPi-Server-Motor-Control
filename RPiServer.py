6d31d26389df986635d92efa84160d7f700bd0ef
EPF Student - Evan <evan.pasteau@uwe.ac.uk>
2024-11-06 15:46:47 +0000

    Saving Progress

--- RPiServer.py
+++ RPiServer.py
@@ -3,12 +3,26 @@ if __name__ == "__main__" :
 
 import socket
 import time
+import os
 
-buffer_size = 1024
+import dotenv # pip install python-dotenv
+
+
+dotenv.load_dotenv()
+
+bufferSize = 1024
 serverPort = 2222
-serverIP = '192.168.0.152'
+serverIP = os.getenv('serverIP_env')
+
+RPi_Socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # Using UTP
+RPi_Socket.bind((serverIP,serverPort))
+
+print('Server is Up')
+
+messageReceived, clientAddress = RPi_Socket.recvfrom(bufferSize)
+messageReceived = messageReceived.decode('utf-8')
+print(f'The message is :\t{messageReceived}\nFrom : \t{clientAddress[0]}\nOn port number {clientAddress[1]}')
+
 messageFromServer = 'Hello World'
-
-
-
-
+messageFromServer_bytes = messageFromServer.encode('utf-8')
+RPi_Socket.sendto(messageFromServer_bytes, clientAddress)
