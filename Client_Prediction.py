8c7dfc2adbf2d12c0faf3f6df74b826f0c1c3e0d
EPF Student - Evan <evan.pasteau@uwe.ac.uk>
2024-11-07 15:06:06 +0000

    Added file to separate progress

---
+++ Client_Prediction.py
@@ -0,0 +1,119 @@
+if __name__ == "__main__" :
+    print("\033cStarting ...\n") # Clear Terminal
+
+import os
+import sys
+import time
+
+try :
+    import torch
+    import torchvision.transforms as transforms
+    from torch.utils.data import DataLoader
+    import dotenv
+except ModuleNotFoundError as Err:
+    missing_module = str(Err).replace('No module named ', '')
+    missing_module = missing_module.replace("'", '')
+    sys.exit(f'No module named {missing_module} try : pip install {missing_module}')
+
+try :
+    from Imports.InferenceDataloader import HAR_Inference_DataSet
+    from Imports.Functions import model_exist
+    from Imports.Models.MoViNet.config import _C as config
+    from Imports.Models.fusion import FusionModel
+except ModuleNotFoundError :
+    sys.exit('Missing Import folder, make sure you are in the right directory')
+
+# Modifiable variables
+action_to_idx = {'down': 0, 'grab': 1, 'walk': 2}   # Action to index mapping
+root_directory = 'Temporary Data'                   # Directory where temporary folders are stored
+time_for_prediction = 25                            # Time we wait for each prediction
+prediction_threshold = 3                            # how much prediction we need to activate
+
+# If there is no model to load, we stop
+if not model_exist() :
+    sys.exit("No model to load")
+try :
+    if not os.listdir(root_directory) :sys.exit('No data to make prediction on, launch GetData.py first')
+except FileNotFoundError :
+    sys.exit('No data to make prediction on, launch GetData.py first')
+
+idx_to_action = {v: k for k, v in action_to_idx.items()}    # We invert the dictionary to have the action with the index
+tracking = []
+
+transform = transforms.Compose([transforms.Resize((224, 224)),transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+dataset = HAR_Inference_DataSet(root_dir=root_directory, transform=transform)
+
+ModelToLoad_Path = os.path.join('Model to Load',os.listdir('./Model to Load')[0])
+ModelName = os.listdir('./Model to Load')[0]
+if ModelName.endswith('.pt') :
+    ModelName = ModelName.replace('.pt','')
+else :
+    ModelName = ModelName.replace('.pht','')
+print(f"Loading {ModelName}")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using {device}\n")
+LINE_UP = '\033[1A'
+LINE_CLEAR = '\x1b[2K'
+
+model = FusionModel(config.MODEL.MoViNetA0, num_classes=3, lstm_input_size=12, lstm_hidden_size=512, lstm_num_layers=2)
+model.load_state_dict(torch.load(ModelToLoad_Path, weights_only = True, map_location=device))
+model.to(device)
+model.eval()
+
+try :
+    print(f'\033cProgramme running   ctrl + C to stop\n\nLoading {ModelName}\nUsing {device}\n')
+    old_sample = ''
+    first_sample = ''
+    for action in action_to_idx:
+        tracking.append(0) # We create a variable in the list for each action
+    if not os.listdir(root_directory) :
+        print('No files in root directory')
+        sys.exit(0)
+    while True:
+        while old_sample == dataset.SampleNumber :
+            time.sleep(0.001)
+            dataset = HAR_Inference_DataSet(root_dir=root_directory, transform=transform)
+        old_sample = dataset.SampleNumber
+        loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0, drop_last=True)
+        with torch.no_grad():
+            for video_frames, imu_data in loader:
+                video_frames, imu_data = video_frames.to(device), imu_data.to(device)
+                outputs = model(video_frames, imu_data)
+                predicted = torch.argmax(model(video_frames, imu_data))
+                tracking[predicted] += 1
+
+        print(f'{old_sample} : {idx_to_action.get(predicted.item())}')
+        if first_sample == '' : first_sample = old_sample
+
+
+except KeyboardInterrupt:
+    num_of_predictions = 0
+    for i in tracking :
+        num_of_predictions += i
+    num_first = int(first_sample.replace('Sample_',''))
+    num_last = int(old_sample.replace('Sample_',''))
+
+    if num_of_predictions > 1 : end_text = 's'
+    else : end_text = ''
+    print(f'\nThere were a total of {num_of_predictions} prediction{end_text}, with {(num_last-num_first+1)-num_of_predictions} missed')
+    for action, i in action_to_idx.items() :
+        print(f'{tracking[i]} for {action}')
+
+except FileNotFoundError:
+    print("Samples folder got deleted")
+    num_of_predictions = 0
+    for i in tracking :
+        num_of_predictions += i
+    num_first = int(first_sample.replace('Sample_',''))
+    num_last = int(old_sample.replace('Sample_',''))
+
+    if num_of_predictions > 1 : end_text = 's'
+    else : end_text = ''
+    print(f'\nThere were a total of {num_of_predictions} prediction{end_text}, with {(num_last-num_first+1)-num_of_predictions} missed')
+    for action, i in action_to_idx.items() :
+        print(f'{tracking[i]} for {action}')
+
+
+
+print('\nProgramme Stopped\n')
---
+++ RPiServer_MotorControl.py
@@ -0,0 +1,201 @@
+if __name__ == "__main__" :
+    print("\033cStarting ...\n") # Clear Terminal
+
+import socket
+import time
+import sys
+import os
+
+try :
+    import dotenv
+    from dynamixel_sdk import *
+except ModuleNotFoundError as Err:
+    missing_module = str(Err).replace('No module named ', '')
+    missing_module = missing_module.replace("'", '')
+    if missing_module == 'dynamixel_sdk':
+        sys.exit(f'No module named {missing_module} try : pip install dynamixel-sdk')
+    elif missing_module == 'dotenv':
+        sys.exit(f'No module named {missing_module} try : pip install python-dotenv')
+    else:
+        print(f'No module named {missing_module} try : pip install {missing_module}')
+
+# -------------------------     # Modifiable variables
+Fixed_Serial_Port = False       # Set to True if you know the serial port you are connected
+Serial_Port = '/dev/ttyUSB0'    # If Fixed_Serial_Port is True connect to this port
+# -------------------------
+
+# -------------------------     # Dynamixel variables
+DXL_ID = 1                      # Dynamixel Motor ID
+BAUD_RATE = 57600               # Communication Baud Rate
+PROTOCOL_VERSION = 1.0          # Dynamixel Protocol version
+ADDR_MX_PRESENT_POSITION = 36   # Address of current position
+ENCODER_COUNTS_PER_REV = 4096   # Number of ticks (1 turn = 4096 ticks)
+# -------------------------
+
+LINE_UP = '\033[1A'
+LINE_CLEAR = '\x1b[2K'
+
+if not Fixed_Serial_Port:
+    os_name = platform.system()
+    if os_name == 'Linux':
+        os_port_name = '/dev/ttyUSB'
+    elif os_name == 'Windows':
+        os_port_name = 'COM'
+    elif os_name == 'Darwin':  # This is Mac OS
+        os_port_name = '/dev/tty.usbserial-'
+    else:
+        sys.exit('Unsupported OS')
+
+    Serial_Connected = False
+    for i in range(1000):
+        try:
+            portHandler = PortHandler(f'{os_port_name}{i}')
+            portHandler.openPort()
+            Serial_Connected = True
+            print(f"\033cSerial Connected at Port {os_port_name}{i}")
+            break
+        except:
+            pass
+    if not Serial_Connected:
+        sys.exit("Serial not connected")
+else:
+    try:
+        portHandler = PortHandler(Serial_Port)
+        portHandler.openPort()
+        Serial_Connected = True
+        print(f"\033cSerial Connected at Port {Serial_Port}")
+    except:
+        sys.exit('Serial not connected or wrong port name')
+
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+# Set Baud Rate
+if portHandler.setBaudRate(BAUD_RATE):
+    print(f"Baud Rate fixed at {BAUD_RATE}\n")
+else:
+    sys.exit("Could not configure Baud Rate")
+
+
+def set_motor_speed(speed):
+    if speed < 0:
+        speed = -speed | 1024  # bitwise OR 1020 for negative speed
+
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, 32, speed)  # Address 32 is for speed control
+
+
+def read_motor_position(inTick=False):
+    dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID,
+                                                                                   ADDR_MX_PRESENT_POSITION)
+    if dxl_comm_result != COMM_SUCCESS:
+        print(f"COMM Error : {packetHandler.getTxRxResult(dxl_comm_result)}")
+    elif dxl_error != 0:
+        print(f"dxl Error : {packetHandler.getRxPacketError(dxl_error)}")
+    else:
+        if inTick:
+            return dxl_present_position
+        else:
+            return dxl_present_position / ENCODER_COUNTS_PER_REV
+
+
+def move_motor(goalTurns):
+    done = False
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION, 0)  # Torque release
+    initialPosition = read_motor_position(inTick=False)
+    previousPosition = 0
+    totalTurns = 0
+
+    while not done:
+        # Set to wheel mode
+        packetHandler.write2ByteTxRx(portHandler, DXL_ID, 6, 0)  # Address of min value is 6
+        packetHandler.write2ByteTxRx(portHandler, DXL_ID, 8, 0)  # Address of max value is 8
+
+        currentPosition = read_motor_position(inTick=False) - initialPosition
+        positionDifference = (currentPosition - previousPosition) * 0.9
+        if positionDifference > 0.8:
+            pass
+        elif positionDifference < -0.8:
+            pass
+        else:
+            totalTurns += positionDifference
+        previousPosition = currentPosition
+
+        print(LINE_UP, end=LINE_CLEAR)
+        print(f'Motor Position : {totalTurns:.2f}\tGoal Position : {goalTurns}')
+
+        if round(totalTurns, 2) == round(goalTurns, 2):
+            set_motor_speed(0)
+            print(LINE_UP, end=LINE_CLEAR)
+            packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION, 0)  # Torque release
+            done = True
+            if goalTurns < 0:
+                direction = 'down'
+                goalTurns = str(goalTurns).replace('-', '')
+            else:
+                direction = 'up'
+            print(f'The motor went {direction} {goalTurns} turn.')
+        else:
+            if goalTurns < 0:
+                motor_speed = max(round((goalTurns - totalTurns) * 1000), -400)
+            else:
+                motor_speed = min(round((goalTurns - totalTurns) * 1000), 400)
+            set_motor_speed(motor_speed)
+
+dotenv.load_dotenv()
+
+bufferSize = 1024
+serverPort = int(os.getenv('serverPort_env'))
+serverIP = os.getenv('serverIP_env')
+
+RPi_Socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # Using UTPy
+RPi_Socket.bind((serverIP,serverPort))
+
+
+
+try :
+    Done = False
+    print('\033c')
+    while not Done :
+        print('Server is Up and waiting ...')
+        messageReceived, clientAddress = RPi_Socket.recvfrom(bufferSize)
+        messageReceived = messageReceived.decode('utf-8')
+        print(LINE_UP,end=LINE_CLEAR)
+        print(f'The message is : {messageReceived}')#\nFrom : \t\t\t{clientAddress[0]}\nOn port number {clientAddress[1]}')
+
+        if messageReceived.lower() == 'done' :
+            messageFromServer = 'Done Received'
+            messageFromServer_bytes = messageFromServer.encode('utf-8')
+            RPi_Socket.sendto(messageFromServer_bytes, clientAddress)
+
+            Done = True
+
+        elif messageReceived.lower() == 'grab' :
+            messageFromServer = f'Grab Received'
+            messageFromServer_bytes = messageFromServer.encode('utf-8')
+            RPi_Socket.sendto(messageFromServer_bytes, clientAddress)
+
+            move_motor(1)
+
+        elif messageReceived.lower() == 'walk' :
+            messageFromServer = f'Walk Received'
+            messageFromServer_bytes = messageFromServer.encode('utf-8')
+            RPi_Socket.sendto(messageFromServer_bytes, clientAddress)
+
+            # Torque lock
+
+        elif messageReceived.lower() == 'down' :
+            messageFromServer = f'Down Received'
+            messageFromServer_bytes = messageFromServer.encode('utf-8')
+            RPi_Socket.sendto(messageFromServer_bytes, clientAddress)
+
+            move_motor(-1)
+
+        else :
+            messageFromServer = f'Unknown Message Received'
+            messageFromServer_bytes = messageFromServer.encode('utf-8')
+            RPi_Socket.sendto(messageFromServer_bytes, clientAddress)
+
+except KeyboardInterrupt :
+    pass
+
+print('Programme Stopped')
+
