11730511b5fcf367f3277d31f2abcb003f8e805b
EPF Student - Evan <evan.pasteau@uwe.ac.uk>
2024-11-07 15:13:11 +0000

    Finished first version of prediction client

--- Client_Prediction.py
+++ Client_Prediction.py
@@ -4,6 +4,7 @@ if __name__ == "__main__" :
 import os
 import sys
 import time
+import socket
 
 try :
     import torch
@@ -13,7 +14,9 @@ try :
 except ModuleNotFoundError as Err:
     missing_module = str(Err).replace('No module named ', '')
     missing_module = missing_module.replace("'", '')
-    sys.exit(f'No module named {missing_module} try : pip install {missing_module}')
+    if missing_module == 'dotenv' :
+        sys.exit(f'No module named {missing_module} try : pip install python-dotenv')
+    else : sys.exit(f'No module named {missing_module} try : pip install {missing_module}')
 
 try :
     from Imports.InferenceDataloader import HAR_Inference_DataSet
@@ -28,6 +31,18 @@ action_to_idx = {'down': 0, 'grab': 1, 'walk': 2}   # Action to index mapping
 root_directory = 'Temporary Data'                   # Directory where temporary folders are stored
 time_for_prediction = 25                            # Time we wait for each prediction
 prediction_threshold = 3                            # how much prediction we need to activate
+
+dotenv.load_dotenv()
+
+bufferSize = 1024
+serverPort = int(os.getenv('serverPort_env'))
+serverIP = os.getenv('serverIP_env')
+serverAddress = (serverIP,serverPort)
+
+UDPClient = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+LINE_UP = '\033[1A'
+LINE_CLEAR = '\x1b[2K'
 
 # If there is no model to load, we stop
 if not model_exist() :
@@ -53,8 +68,6 @@ print(f"Loading {ModelName}")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using {device}\n")
-LINE_UP = '\033[1A'
-LINE_CLEAR = '\x1b[2K'
 
 model = FusionModel(config.MODEL.MoViNetA0, num_classes=3, lstm_input_size=12, lstm_hidden_size=512, lstm_num_layers=2)
 model.load_state_dict(torch.load(ModelToLoad_Path, weights_only = True, map_location=device))
@@ -63,6 +76,7 @@ model.eval()
 
 try :
     print(f'\033cProgramme running   ctrl + C to stop\n\nLoading {ModelName}\nUsing {device}\n')
+    Done = False
     old_sample = ''
     first_sample = ''
     for action in action_to_idx:
@@ -70,7 +84,7 @@ try :
     if not os.listdir(root_directory) :
         print('No files in root directory')
         sys.exit(0)
-    while True:
+    while not Done:
         while old_sample == dataset.SampleNumber :
             time.sleep(0.001)
             dataset = HAR_Inference_DataSet(root_dir=root_directory, transform=transform)
@@ -83,8 +97,21 @@ try :
                 predicted = torch.argmax(model(video_frames, imu_data))
                 tracking[predicted] += 1
 
-        print(f'{old_sample} : {idx_to_action.get(predicted.item())}')
+        message = f'{old_sample} : {idx_to_action.get(predicted.item())}'
+        print(message)
         if first_sample == '' : first_sample = old_sample
+
+        messageFromClient = str(message)
+        messageFromClient_bytes = messageFromClient.encode('utf-8')
+        UDPClient.sendto(messageFromClient_bytes, serverAddress)
+
+        dataReceived ,serverAddressReceived = UDPClient.recvfrom(bufferSize)
+        dataReceived = dataReceived.decode('utf-8')
+        print(f'Message From Server : {dataReceived}')#\nFrom : \t\t\t{serverAddressReceived[0]}\nOn port number {serverAddressReceived[1]}')
+        if dataReceived == 'Done Received' :
+            Done = True
+
+
 
 
 except KeyboardInterrupt:
