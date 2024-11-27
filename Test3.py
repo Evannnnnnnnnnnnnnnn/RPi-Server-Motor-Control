feb7d07e3960e4b6b4c989dc0e7167272e5f3ff7
Evan <evan.pasteau@gmail.com>
2024-11-27 16:33:26 +0000

    Changed message

--- Test3.py
+++ Test3.py
@@ -3,9 +3,6 @@ import dotenv
 import os
 import sys
 
-bufferSize = 1024
-
-
 dotenv.load_dotenv()
 
 bufferSize = 1024
@@ -16,9 +13,12 @@ except TypeError :
     sys.exit('\033cPlease open .env.shared and follow instructions')
 
 RPi_Socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # Using UTPy
-RPi_Socket.settimeout(10)
+RPi_Socket.settimeout(3)
 RPi_Socket.bind((serverIP,serverPort))
 
-messageReceived, clientAddress = RPi_Socket.recvfrom(bufferSize)
+try :
+    messageReceived, clientAddress = RPi_Socket.recvfrom(bufferSize)
+except TimeoutError:
+    print('Timeout')
 
 print('end')
