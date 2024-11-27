e94bf1fbf629ccec3eefa73976bb51dfad9ee542
Evan <evan.pasteau@gmail.com>
2024-11-27 16:27:41 +0000

    Changed IP back to the correct one

--- Test3.py
+++ Test3.py
@@ -1,8 +1,18 @@
 import socket
+import dotenv
+import os
+import sys
 
 bufferSize = 32
-serverPort = 2222
-serverIP = '164.11.72.166'
+
+dotenv.load_dotenv()
+
+bufferSize = 1024
+try :
+    serverPort = int(os.getenv('serverPort_env'))
+    serverIP = os.getenv('serverIP_env')
+except TypeError :
+    sys.exit('\033cPlease open .env.shared and follow instructions')
 
 RPi_Socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # Using UTPy
 RPi_Socket.bind((serverIP,serverPort))
